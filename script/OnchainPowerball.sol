0f99d2aaec6c89ca0570ff7edf0d8b51dcb071ea
Sardius <0xsardius@gmail.com>
2025-01-06 21:06:13 -0500

    update

--- script/OnchainPowerball.sol
+++ script/OnchainPowerball.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {OnChainPowerball} from "../src/OnchainPowerball.sol";
 
-contract CounterScript is Script {
+contract OnchainPowerballScript is Script {
     Counter public counter;
 
     function setUp() public {}
